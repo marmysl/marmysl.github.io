d601c40a574b0deff0914b7dfdfaf1ba9a5291b9
Maria <mdm153@pitt.edu>
2020-10-15 16:12:08 -0400

    round2

--- js/script.js
+++ js/script.js
@@ -1,14 +1,37 @@
-function revealMsg() {
-	document.getElementById("hiddenMessage").style.display = 'block';
+var form = document.getElementById("modal");
+var close = document.getElementById("close");
+
+var btn = document.getElementById("topbtn");
+window.onscroll = function() {scrollFunction()};
+
+// Show top button after scrolling
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    btn.style.display = "block";
+  } else {
+    btn.style.display = "none";
+  }
 }
 
-function countDown() {
-	var curr = document.getElementById("countDownButton").innerHTML;
-	var newer = curr - 1;
-	if (newer < 0) {
-		newer = 0;
-	}
-	document.getElementById("countDownButton").innerHTML = newer;
-
+// Go to top of the form
+function goToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
 
+// Open contact form
+function openForm() {
+	form.style.display = "block";
+}
+
+// Close form
+function closeForm() {
+	form.style.display = "none";
+}
+
+// Close if clicked out
+window.onclick = function(event) {
+  if (event.target == form) {
+    form.style.display = "none";
+  }
+}
